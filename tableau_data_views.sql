7cf2745c73b715c0afd3f621f72ed9e82bac165e
Kasia <k.moscipan@gmail.com>
2024-12-15 16:32:33 +0100

    Add files via upload

---
+++ tableau_data_views.sql
@@ -0,0 +1,65 @@
+/* SalesDetails View Creation*/
+
+CREATE VIEW SalesDetails AS
+SELECT 
+    fs.DateKey,
+    dc.ChannelName, 
+    fs.StoreKey,
+    fs.ProductKey,
+    fs.TotalCost,
+    fs.SalesAmount,
+    (fs.SalesAmount - fs.TotalCost) AS GrossMargin
+FROM 
+    FactSales fs
+INNER JOIN 
+    DimChannel dc ON fs.ChannelKey = dc.ChannelKey
+WHERE
+    dc.ChannelName = 'Store'; 
+
+/* SalesDetails View Access*/
+SELECT * FROM SalesDetails;
+
+
+
+
+/* ProductDetails View Creation*/
+
+CREATE VIEW ProductDetails AS
+SELECT 
+    dp.ProductKey, 
+    dp.ProductName, 
+    dpsc.ProductSubcategoryName, 
+    dpc.ProductCategoryName 
+FROM 
+    DimProduct dp
+INNER JOIN 
+    DimProductSubcategory dpsc ON dp.ProductSubcategoryKey = dpsc.ProductSubcategoryKey
+INNER JOIN 
+    DimProductCategory dpc ON dpsc.ProductCategoryKey = dpc.ProductCategoryKey;
+
+/* ProductDetails View Access*/
+SELECT * FROM ProductDetails;
+
+
+
+
+/* StoreDetails View Creation*/
+
+CREATE VIEW StoreDetails AS
+SELECT 
+    s.StoreKey,
+    s.StoreName,
+    ISNULL(EmployeeCount, 25) AS EmployeeCount, -- Default EmployeeCount to 25 if NULL
+    s.SellingAreaSize,
+    g.CityName,
+    g.RegionCountryName AS CountryName,
+    g.ContinentName
+FROM 
+    DimStore AS s
+INNER JOIN 
+    DimGeography AS g ON s.GeographyKey = g.GeographyKey
+WHERE 
+    s.StoreType = 'Store';
+
+/* StoreDetails View Access*/
+SELECT * FROM StoreDetails;
